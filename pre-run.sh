be19213d94f35a8785bffd0b08a0fedbaa475ba0
croyla <crola@protonmail.com>
2026-05-21 09:52:28 +0530

    run.sh automated test

---
+++ pre-run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cutoff=$(date -v-2m +%Y%m%d 2>/dev/null || date -d '2 months ago' +%Y%m%d)
+for f in db/database-*.db; do
+  [[ "$f" =~ db/database-([0-9]{8})- ]] || continue
+  [[ "${BASH_REMATCH[1]}" < "$cutoff" ]] && rm "$f"
+done
